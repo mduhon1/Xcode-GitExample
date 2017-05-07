6c762d63be6e7f9d3d4fa2df00c8ec5d59be6239
Michael Duhon <mduhon1@mduhon1.local>
2017-05-07 17:19:05 -0500

    Initial Commit

---
+++ GitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExample
+//
+//  Created by Michael Duhon on 5/7/17.
+//  Copyright © 2017 Michael Duhon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
